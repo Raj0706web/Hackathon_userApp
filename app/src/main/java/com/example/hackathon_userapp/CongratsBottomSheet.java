7ea335aca3077284497fb003eccb7792822792d3
Light-Ak <ak26425617@gmail.com>
2025-03-01 15:28:25 +0530

    adapter.

---
+++ app/src/main/java/com/example/hackathon_userapp/CongratsBottomSheet.java
@@ -0,0 +1,22 @@
+package com.example.hackathon_userapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class CongratsBottomSheet extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_congrats_bottom_sheet, container, false);
+    }
+}
